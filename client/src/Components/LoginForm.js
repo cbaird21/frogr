83ecbe360b5724092f3cd9ea614d2d63574a5fd4
unknown <chandler.baird21@gmail.com>
2023-01-17 17:37:26 -0700

    added login form and sign up form to components

---
+++ client/src/Components/LoginForm.js
@@ -0,0 +1,108 @@
+// see SignupForm.js for comments
+import React, { useState, useEffect } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import { LOGIN_USER } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
+// import { loginUser } from '../utils/API';
+import Auth from "../utils/auth";
+
+const LoginForm = () => {
+    const [userFormData, setUserFormData] = useState({ email: "", password: "" });
+    const [validated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    // Replace the loginUser() functionality imported from the API file with the LOGIN_USER mutation functionality.
+    const [login, { error }] = useMutation(LOGIN_USER);
+
+    useEffect(() => {
+        if (error) {
+            setShowAlert(true);
+        } else {
+            setShowAlert(false);
+        }
+    }, [error]);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({ ...userFormData, [name]: value });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        // check if form has everything (as per react-bootstrap docs)
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        try {
+            // Replace the login() functionality imported from the API file with the LOGIN_USER mutation functionality.
+            const { data } = await login({
+                variables: { ...userFormData },
+            });
+            // grab keys from mutation
+            Auth.login(data.login.token);
+        } catch (err) {
+            console.error(err);
+        }
+        // reset login form fields
+        setUserFormData({
+            email: "",
+            password: "",
+        });
+    };
+
+    return (
+        <>
+            <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+                <Alert
+                    dismissible
+                    onClose={() => setShowAlert(false)}
+                    show={showAlert}
+                    variant="danger"
+                >
+                    Something went wrong with your login credentials!
+                </Alert>
+                <Form.Group>
+                    <Form.Label htmlFor="email">Email</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Your email"
+                        name="email"
+                        onChange={handleInputChange}
+                        value={userFormData.email}
+                        required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        Email is required!
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label htmlFor="password">Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Your password"
+                        name="password"
+                        onChange={handleInputChange}
+                        value={userFormData.password}
+                        required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        Password is required!
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Button
+                    disabled={!(userFormData.email && userFormData.password)}
+                    type="submit"
+                    variant="success"
+                >
+                    Submit
+                </Button>
+            </Form>
+        </>
+    );
+};
+
+export default LoginForm;
