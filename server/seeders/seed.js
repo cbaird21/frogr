1aa21831f843cc1b2c628ad9c7169a00fcb6bdc7
GarrettA01 <gman.anderson2001@gmail.com>
2023-01-19 11:57:59 -0700

    Changed seed.js name

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,7 +1,7 @@
 const db = require("../config/connection");
 const { User, Post } = require("../models");
 const userSeeds = require("./userSeeds.json");
-const thoughtSeeds = require("./thoughtSeeds.json");
+const postSeeds = require("./postSeeds.json");
 
 db.once("open", async () => {
   try {
