08df7679fa538ea96527c14e5c582b047e405a8e
Jenn Marshall <jennyfever@gmail.com>
2023-01-19 12:17:05 -0700

    trying some stuff with the upload widget

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import "../css/styles.css";
 
 import Auth from '../../utils/auth';
 import NavBar from '../NavBar';
--- client/src/Components/UploadWidget.js
+++ client/src/Components/UploadWidget.js
@@ -1,18 +1,31 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { ADD_POST } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
 
 const UploadWidget = () => {
   const cloudinaryRef = useRef();
   const widgetRef = useRef();
+  const [imageId, setImageId] = useState("");
+  const [addPost, {error}] = useMutation(ADD_POST);
   useEffect(() => {
     cloudinaryRef.current = window.cloudinary;
-    console.log(cloudinaryRef.current);
     widgetRef.current = cloudinaryRef.current.createUploadWidget(
       {
         cloudName: "dpfqzf6l7",
         uploadPreset: "wohj6xxg",
       },
-      function (error, result) {
-        console.log(result);
+      async function (error, result) {
+        if (result.event === "success") {
+            console.log(result.info.public_id)
+            setImageId(result.info.public_id);
+        }
+        try {
+            const {data} = await addPost({
+                variables: {postImage: { imageId }}
+            })
+        } catch (err) {
+            console.error(err);
+        }
       }
     );
   }, []);
