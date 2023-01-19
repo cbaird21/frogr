737ebe8c8b915bc054b7ab53cffab28b67e26e75
Andy LaBorde <32881770+AndyLaBorde@users.noreply.github.com>
2023-01-18 19:22:51 -0700

    installed dependencies for cloudinary

--- client/src/pages/Discover.js
+++ client/src/pages/Discover.js
@@ -4,44 +4,50 @@
 import React from 'react'
 import {AdvancedImage} from '@cloudinary/react';
 import {Cloudinary} from "@cloudinary/url-gen";
+import {scale} from "@cloudinary/url-gen/actions/resize";
 // Import any actions required for transformations.
 import {fill} from "@cloudinary/url-gen/actions/resize";
 import { Container, Card } from 'react-bootstrap';
 import CardHeader from 'react-bootstrap/esm/CardHeader';
+require('dotenv').config()
 
 const Discover = () => {
-  // 2. Set your cloud name
-  //========================
-  // Create a Cloudinary instance and set your cloud name.
+    // 2. Set your cloud name
+    //========================
+    // Create a Cloudinary instance and set your cloud name.
     const cld = new Cloudinary({
         cloud: {
-        cloudName: 'drxesxzoi'
+            cloudName: 'drxesxzoi',
+
+
         }
     });
     // 3. Get your image
     //===================
     // Instantiate a CloudinaryImage object for the image with the public ID, 'docs/models'.
-    const myImage = cld.image('images'); 
+    const myImage = cld.image('images/frog1'); 
     // 4. Transform your image
     //=========================
     // Resize to 250 x 250 pixels using the 'fill' crop mode.
-    myImage.resize(fill().width(250).height(250)).format('png');
+    myImage.resize(scale().width(250).height(250)).format('png');
     // 5. Deliver your image
     // =========================
     // Render the image in a React component.
+    
     return (
+        
         <Container fluid>
             <h1>This is the discover page</h1>
             <Card className='m-3' style={{ width: '18rem' }}>
                 <CardHeader>
                     <img alt="profile pic"></img><h3>Username</h3>
                 </CardHeader>
-                <Card Body>
+                <Card.Body>
                     <AdvancedImage cldImg={myImage} />
-                </Card>
-                <Card.Text>
-                    post description
-                </Card.Text>
+                    <Card.Text>
+                        post description
+                    </Card.Text>
+                </Card.Body>
                 <Card.Footer>
                     <small className="text-muted">createdAt</small>
                 </Card.Footer>
