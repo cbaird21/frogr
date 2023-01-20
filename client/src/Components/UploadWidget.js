89486b5d458b50826bec8d0e3ab600c458f15d5d
unknown <chandler.baird21@gmail.com>
2023-01-20 13:53:58 -0700

    added corrections

--- client/src/Components/UploadWidget.js
+++ client/src/Components/UploadWidget.js
@@ -9,42 +9,43 @@ const UploadWidget = () => {
   if (error) {
     console.log(JSON.stringify(error))
   }
-//   const [imageId, setImageId] = useState("");
-//   let imageChange = false;
+  //   const [imageId, setImageId] = useState("");
+  //   let imageChange = false;
   useEffect(() => {
-      cloudinaryRef.current = window.cloudinary;
-      widgetRef.current = cloudinaryRef.current.createUploadWidget(
-          {
-              cloudName: "dpfqzf6l7",
-              uploadPreset: "wohj6xxg",
-            },
-            async function (error1, result) {
-                if (error1) {
-                    console.log(error1)
-                }
+    cloudinaryRef.current = window.cloudinary;
+    widgetRef.current = cloudinaryRef.current.createUploadWidget(
+      {
+        cloudName: "dpfqzf6l7",
+        uploadPreset: "wohj6xxg",
+      },
+      async function (error1, result) {
+        if (error1) {
+          console.log(error1)
+        }
         // console.log(result)
         if (result.event === "success") {
           console.log(result.info.url);
           const imageUrl = result.info.url
           try {
-        //       setImageId(result.info.url);
-              const { data } = await addPost({
-                  variables: { postImage: imageUrl },
-                // variables: { postImage: { imageId } },
-                }); 
-                console.log(data);
-            } catch (err) {
-                console.error(JSON.stringify(err));
-            }
+            //       setImageId(result.info.url);
+            // note
+            const { data } = await addPost({
+              variables: { postImage: imageUrl },
+              // variables: { postImage: { imageId } },
+            });
+            console.log(data);
+          } catch (err) {
+            console.error(JSON.stringify(err));
+          }
         }
-    }
+      }
     );
-}, []);
+  }, []);
 
-// console.log(imageId);
-// const addImage = async (error, result) => {
+  // console.log(imageId);
+  // const addImage = async (error, result) => {
 
-// }
+  // }
   return <button onClick={() => widgetRef.current.open()}>Upload</button>;
 };
 
