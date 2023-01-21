88f343d1919382cdfe8430e55f85fbbe4144bf93
Andy LaBorde <32881770+AndyLaBorde@users.noreply.github.com>
2023-01-21 11:44:40 -0700

    profile and discover pages with working masony, adjusted footer scss

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -6,7 +6,7 @@ const Footer = () => {
     const location = useLocation();
     const navigate = useNavigate();
     return (
-      <footer className="w-100 mt-auto bg-secondary p-4 fixed-bottom">
+      <footer className="w-100 mt-auto bg-secondary p-4 ">
         <div className="container text-center mb-5">
           {location.pathname !== "/" && (
             <button className="btn btn-dark mb-3" onClick={() => navigate(-1)}>
--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -10,7 +10,7 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header className=" bg-primary text-light mb-4 py-3 flex-row align-center fixed-top">
+    <header className=" bg-primary text-light mb-4 py-3 flex-row align-center">
       <AppNavBar />
     </header>
   );
