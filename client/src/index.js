9bd18a36ea03cfeab7edc6706a78c6215fde94d7
unknown <chandler.baird21@gmail.com>
2023-01-22 21:13:47 -0700

    trying

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import '..';
+import '../src/styles.scss';
 import App from './App';
 
 
