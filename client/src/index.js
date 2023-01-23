cd08f0b14f9c7a8697ea52f9c2d2cc3b504b3afa
unknown <chandler.baird21@gmail.com>
2023-01-22 21:12:27 -0700

    trying to get path working

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import 'client/src/components/css/styles.scss';
+import '..';
 import App from './App';
 
 
